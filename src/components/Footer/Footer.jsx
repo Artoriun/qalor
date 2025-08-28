bb8893ceedae207956834071a9cb4acffb937440
Peter de Keijzer <pjcr.dekeijzer@gmail.com>
2025-08-28 17:21:22 +0900

    Update footer email address to pdk@qalor.nl and deploy to docs

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -152,7 +152,7 @@ const Footer = () => {
           <div>
             <h4 style={{ fontSize: '1.2rem', marginBottom: '1rem', fontFamily: 'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", sans-serif' }}>Contact</h4>
             <div style={{ lineHeight: '2', color: '#666', fontFamily: 'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", sans-serif' }}>
-              <p><a href="mailto:info@qalor.nl" style={{ color: '#666', textDecoration: 'none', outline: 'none' }} onFocus={(e) => e.target.style.outline = 'none'} onBlur={(e) => e.target.style.outline = 'none'}>info@qalor.nl</a></p>
+              <p><a href="mailto:pdk@qalor.nl" style={{ color: '#666', textDecoration: 'none', outline: 'none' }} onFocus={(e) => e.target.style.outline = 'none'} onBlur={(e) => e.target.style.outline = 'none'}>pdk@qalor.nl</a></p>
               <p><a href="tel:06 112 16 938" style={{ color: '#666', textDecoration: 'none', outline: 'none' }} onFocus={(e) => e.target.style.outline = 'none'} onBlur={(e) => e.target.style.outline = 'none'}>06 112 16 938</a></p>
               <p><a href="https://maps.app.goo.gl/svtgb5ivAYVd9MXAA" style={{ color: '#666', textDecoration: 'none', outline: 'none' }} onFocus={(e) => e.target.style.outline = 'none'} onBlur={(e) => e.target.style.outline = 'none'}>Lange Marktstraat 1, 8911AD, Leeuwarden</a></p>
             </div>
